ad5746fefabd7725635bb5dcb650fe96f18ffe0f
aceballos <aceballos@6006247a-2d13-4e52-b961-62b44c6c37c8>
2017-07-24 15:47:33 +0000

    buffer overrun in return_append_str
    git-svn-id: svn://svn.sv.gnu.org/chess/trunk@162 6006247a-2d13-4e52-b961-62b44c6c37c8

--- src/frontend/lexpgn.cc
+++ src/frontend/lexpgn.cc
@@ -2127,7 +2127,7 @@ char *yytext;
 
    GNU Chess frontend
 
-   Copyright (C) 2001-2011 Free Software Foundation, Inc.
+   Copyright (C) 2001-2017 Free Software Foundation, Inc.
 
    GNU Chess is based on the two research programs 
    Cobalt by Chua Kong-Sian and Gazebo by Stuart Cracraft.
